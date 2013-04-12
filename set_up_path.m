ccb25522f09678f4ee51eac945da079219cdcd15
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-12 18:23:16 -0400

    adding some more things to the path

--- set_up_path.m
+++ set_up_path.m
@@ -7,10 +7,12 @@
   disp(sprintf('setting up paths...')) ;
 
   path('lib/audioread', path) ;
+  path('lib/fuf', path) ;
   path('src/preprocessing/', path) ;
   path('src/postprocessing/', path) ;
   path('src/scripts/', path) ;
   path('src/util/', path) ;
-  path('src/wrappers', path) :
+  path('src/wrappers', path) ;
+  addpath(genpath('lib/DeepLearnToolbox'));
 
   disp(sprintf('...done')) ;
