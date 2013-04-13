36ae1f4dc35c8067c12befc57656bb67101e491c
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-12 23:15:36 -0400

    adding some explanation to the top of pretrain_rb

--- src/scripts/pretrain_rbm.m
+++ src/scripts/pretrain_rbm.m
@@ -1,8 +1,14 @@
+% This is an example script to show you how to call the wrapper for pretraining
+% a DBN. The basic idea is to use the utility functions used below to create
+% the parameter objects for the network and the training, and to pass that
+% along with the list of filenames that hold the data to the training
+% wrapper.
+
+% TODO(ben): replace this with reading from sams variables
 % get list of song data with full path names
 songs_list = fuf('/var/data/lenin/matlab/*.mat', 'detail');
 songs_list = songs_list(1:10);
 %songs_list = songs_list(1:total_songs);
-
 
 % network topology
 layer_sizes = [100, 100];
