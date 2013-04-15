e3c933cc5636024769f9c5bea67c4811f3535922
Samuel Messing <samuel.messing@gmail.com>
2013-04-15 14:26:33 -0400

    fixing up comment structure, adding optional preprocessing params structure.

--- src/wrappers/pre_train_dbn.m
+++ src/wrappers/pre_train_dbn.m
@@ -1,54 +1,69 @@
 % function [ dbn ] = pre_train_dbn( network_params, training_params,...
 %    files_to_train )
 %
-% pre_train_dbn pretrains a deep belief network and returns the pretrained
-% network.
+% Pretrain a deep belief network and return the pre-trained network.
 %
-% network_params: Object containing all of the DBN network parameters.
+% Inputs:
+%   network_params : Object containing all of the DBN network parameters.
 %
-%   network_params.layer_sizes: 
-%       a 1 x n vector of scalars where each scalar value is the number of
-%       nodes in that hidden layer. The size of the visible layer is NOT
-%       specified
-%   network_params.gaussian_visible_units:
-%       a 1 indicating the lowest RBM containing the visible inputs should
-%       be a Gaussian-Bernoulli RBM. 0 indicates it is Bernoulli-Bernoulli.
+%     - network_params.layer_sizes : 
+%          a 1 x n vector of scalars where each scalar value is the number of
+%          nodes in that hidden layer. The size of the visible layer is NOT
+%          specified
 %
+%     - network_params.gaussian_visible_units:
+%          a 1 indicating the lowest RBM containing the visible inputs should
+%          be a Gaussian-Bernoulli RBM. 0 indicates it is Bernoulli-Bernoulli.
 %
-% training_params: Object containing the training parameters for the
-%                  pre-training phase.
+%   training_params : Object containing the training parameters for the
+%                    pre-training phase.
 %
-%   training_params.num_epochs:
-%       A scalar. The number of training epochs to perform on each RBM
-%       training
-%   training_params.song_batch_size:
-%       A scalar. The number of songs to concatenate into one and train the
-%       DBN on at a time. This is at a higher level than epochs and 
-%       minibatches.
-%   training_params.mini_batch_size:
-%       A scalar. The number of frames to use when training an RBM layer.
-%       This is the normal meaning of the batchsize parameter, as described
-%       in the literature
-%   training_params.momentum:
-%       A scalar value between 0 and 1. The momentum used in training the
-%       DBN for updating the weights.
-%   training_params.learning_rate:
-%       A small scalar value, e.g., 0.05. The learning rate.
+%     - training_params.num_epochs:
+%         A scalar. The number of training epochs to perform on each RBM
+%         training
 %
+%     - training_params.song_batch_size:
+%         A scalar. The number of songs to concatenate into one and train the
+%         DBN on at a time. This is at a higher level than epochs and 
+%         minibatches.
 %
-% files_to_train: A cell array, where each element is a string containing
-%                 the full pathname of a song matlab variable. The loaded
-%                 object file has the following structure:
+%     - training_params.mini_batch_size:
+%         A scalar. The number of frames to use when training an RBM layer.
+%         This is the normal meaning of the batchsize parameter, as described
+%         in the literature
+%
+%     - training_params.momentum:
+%         A scalar value between 0 and 1. The momentum used in training the
+%         DBN for updating the weights.
+%
+%     - training_params.learning_rate:
+%         A small scalar value, e.g., 0.05. The learning rate.
+%
+%   files_to_train : A cell array, where each element is a string containing
+%                    the full pathname of a song matlab variable. The loaded
+%                    object file has the following structure:
+%
 %                       samples: [d x n double]
 %                         freqs: [d x 1 double]
 %                    timestamps: [1 x n double]
 %                      filename: '17-Julia.mp3'
 %                        labels: [1 x n double]
 %
-% dbn: A DBN object, as used by the framework, already pretrained.
+%   opt_preprocessing_params : Optional object containing the preprocessing
+%                              parameters for the data.
 %
-function [ dbn ] = pre_train_dbn( network_params, training_params,...
-    files_to_train )
+%     - preprocessing_params.epsilon:
+%         A scalar. The regularization term used using PCA whitening.
+%
+%     - preprocessing_params.opt_k:
+%         A scalar (integer). The number of dimensions to project the data
+%         onto.  If unspecified no dimensionality reduction is done.
+%
+% Ouputs:
+%   dbn : A DBN object, as used by the framework, already pretrained.
+%
+function [ dbn ] = pre_train_dbn( network_params, training_params, ...
+    files_to_train, opt_preprocessing_params )
 fprintf('%s\n', 'Setting up DBN.');
 
 % setup network parameters
@@ -87,7 +102,8 @@ for b = 1 : num_song_batches
     % first_ind == last_ind then we don't get a cell array but just a string
     % (or something else who the hell knows), which breaks load_songs which
     % uses cell indexing. Looks weird but does the trick.
-    [ train_x, ~, ~ ] = load_songs({files_to_train{rand_song_order(:, first_ind:last_ind)}}) ;
+    [ train_x, ~, ~ ] = load_songs(...
+        { files_to_train{rand_song_order(:, first_ind:last_ind)} }) ;
 
     train_x = train_x'; % convert from d x n to n x d matrix.
 
