e95cbc9e8dd7f4ad12eb78caed741703c753cb21
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-17 16:18:50 -0400

    train script changes

--- src/scripts/call_train_script.m
+++ src/scripts/call_train_script.m
@@ -5,15 +5,15 @@
 
 % data
 run = 1; % we will just do run #1 to start, and do others on good models
-window_size = 1024;
-window_overlap = 512;
+window_size = 2 ^ 15;
+window_overlap = 2 ^ 14;
 preprocessing_epsilon = .00001;
-preprocessing_k = 256;   
+preprocessing_k = 275;   
 % dbn
 dbn_train_percentage = 30; % 30, 60, or 90
-dbn_layer_sizes = [50];
+dbn_layer_sizes = [500 250];
 dbn_is_visible_layer_gaussian = 1;
-dbn_num_epochs = 10;
+dbn_num_epochs = 100;
 dbn_song_batch_size = 10;
 dbn_mini_batch_size = 100;
 dbn_momentum = .7;
@@ -21,7 +21,7 @@ dbn_binary_learning_rate = .01;
 dbn_gaussian_learning_rate = .0003;
 % nn
 nn_train_percentage = 30; % 30, 60, or 90. But keep it low.
-nn_num_epochs = 10;
+nn_num_epochs = 100;
 nn_batch_size = 100;
 nn_learning_rate = 1;
 nn_activation_function = 'sigm';
@@ -29,11 +29,11 @@ nn_momentum = .6;
 nn_plot = 1;
 nn_output = 'softmax';
 nn_scaling_learning_rate = .99;
-nn_weight_penalty_L2 = .1;
+nn_weight_penalty_L2 = .001;
 nn_non_sparsity_penalty = .1;
 nn_sparsity_target = .05; % does nothing if above is set to 0
 nn_input_zero_masked_fraction = 0; % only non-zero for autoencoders
-nn_dropout_fraction = 0;
+nn_dropout_fraction = .5;
 
 % train the dbn and nn
 model_filename = create_train_save_beatles_dbn_nn_model(...
@@ -55,8 +55,8 @@ model_filename = create_train_save_beatles_dbn_nn_model(...
 % function the filename of the model, including the .mat, but do not
 % include the path. 
 %
-% Look at layers param to this function to see how to select which layers
-% to use.
+% Look at layers param to this function to see how to select only certain
+% layers from the NN
 convert_trained_dbn_to_svm(model_filename);
 
 
