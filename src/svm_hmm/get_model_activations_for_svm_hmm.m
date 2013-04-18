bdaa446031db4443dea738ecaf10f486ea8cadb2
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-17 23:54:06 -0400

    adding debug statements and trying to clear unneeded variables to deal with thrashing

--- src/svm_hmm/get_model_activations_for_svm_hmm.m
+++ src/svm_hmm/get_model_activations_for_svm_hmm.m
@@ -30,24 +30,33 @@ if nargin == 1
 end
 
 % load song data
+disp('Loading raw training song data');
 [train_nn_x, train_song_borders, train_nn_y] = load_songs(model.train_nn_songs);
-[test_nn_x, test_song_borders, test_nn_y] = load_songs(model.test_nn_songs);
+disp('Converting training labels to one hot');
 train_labels = one_hot_to_flat_labels(train_nn_y);
-test_labels = one_hot_to_flat_labels(test_nn_y);
-
-% whiten data
+disp('Whitening training data');
 train_nn_x = whiten_data(train_nn_x, model.preprocessing_params.X_avg,...
     model.preprocessing_params.W);
+
+disp('Loading raw testing song data');
+[test_nn_x, test_song_borders, test_nn_y] = load_songs(model.test_nn_songs);
+disp('Converting testing labels to one hot');
+test_labels = one_hot_to_flat_labels(test_nn_y);
+disp('Whitening testing data');
 test_nn_x = whiten_data(test_nn_x, model.preprocessing_params.X_avg,...
     model.preprocessing_params.W);
 
 % put data through network by calling predict and concat layers together
+disp('Computing activations for training data');
 [~, nn] = nnpredict(model.nn, train_nn_x');
+clear train_nn_x;
 train_features_cat = horzcat(nn.a{layers})';
 [train_features, train_labels] = split_data_into_song_cells(...
     train_features_cat, train_labels, train_song_borders );
 
+disp('Computing activations for testing data');
 [~, nn] = nnpredict(model.nn, test_nn_x');
+clear test_nn_x;
 test_features_cat = horzcat(nn.a{layers})';
 [test_features, test_labels] = split_data_into_song_cells(...
     test_features_cat, test_labels, test_song_borders );
