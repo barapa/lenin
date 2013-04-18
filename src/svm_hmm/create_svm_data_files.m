f7e965c00104c93442fe51ee766180e588cd0e7d
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-18 01:07:17 -0400

    fixing bug with passing single song in to svm writer, need to tell it the song number as well

--- src/svm_hmm/create_svm_data_files.m
+++ src/svm_hmm/create_svm_data_files.m
@@ -1,4 +1,5 @@
-% function [ ] = create_svm_data_files( outfile_dir, features, labels, song_names )
+% function [ ] = create_svm_data_files( outfile_dir, features, labels,...
+%   song_names, song_num)
 %
 % Writes the data file and labels file to the given outfile_dir.
 % This can be called for each song that will end up in the same file, just
@@ -16,6 +17,9 @@
 %               is the number of frames in the song. Each element in the
 %               vector is a chord label, numbered starting at zero.
 % song_names:   A 1 x S cell array where each cell is a string with the song name
+% song_num:     (optional) integer to include only in the case of passing this
+%               function a single song, so you can tell it what song number it
+%               is.
 
 % The format of the output file is:
 %   -TAG qid:EXNUM FEATNUM:FEATVAL FEATNUM:FEATVAL ... #SONG_NAME
@@ -24,7 +28,8 @@
 %          index into the the features cell array that the song comes from
 %   -SONG_NAME is the name of the song, and appears in the comment
 
-function [ ] = create_svm_data_files( outfile_dir, features, labels, song_names )
+function [ ] = create_svm_data_files( outfile_dir, features, labels,...
+  song_names, song_num)
 
 num_songs = numel(features);
 dim = size(features{1}, 1);
@@ -46,7 +51,11 @@ for song = 1 : num_songs
     % song labels must start with 1...so add 1 to each
     song_labels = song_labels + 1;
     for frame = 1 : numel(song_labels)
-      chord_label = sprintf('%d', song_labels(frame));
+      if nargin == 5
+        chord_label = sprintf('%d', song_num);
+      else
+        chord_label = sprintf('%d', song_labels(frame));
+      end
       song_label = sprintf(' qid:%d ', song);
       features_ind_vec = reshape([1:dim; song_features(:,frame)'],1,[]);
       features_str = sprintf('%d:%.9f ', features_ind_vec);
