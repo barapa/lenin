9b1d34411095110a2b578934a131083f2bcab88a
Samuel Messing <samuel.messing@gmail.com>
2013-04-19 15:31:55 -0400

    updating godel call script with new vars.

--- src/scripts/godel_call_train_script.m
+++ src/scripts/godel_call_train_script.m
@@ -1,39 +1,44 @@
 % FIRST SECTION IS TRAINING THE DBN AND NN AND SAVING THE RESULTS. 
 % SECOND SECTION IS WRITING THE ACTIVATIONS TO FILES FOR SVM TRAINING.
 
+% set environment global vars
+LENIN_DATA_DIR = 'var/data/lenin';
+global LENIN_DATA_DIR;
+
 % TRAINING DBN AND NN
 
 % data
 run = 1; % we will just do run #1 to start, and do others on good models
 window_size = 1024;
-window_overlap = 128;
+window_overlap = 512;
 preprocessing_epsilon = .00001;
-preprocessing_k = 275;   
+preprocessing_k = 512;
 % dbn
 dbn_train_percentage = 30; % 30, 60, or 90
-dbn_layer_sizes = [50 50 50];
+dbn_layer_sizes = [200 50];
 dbn_is_visible_layer_gaussian = 1;
-dbn_num_epochs = 1;
-dbn_song_batch_size = 1;
-dbn_mini_batch_size = 1;
-dbn_momentum = .7;
-dbn_binary_learning_rate = .01;
-dbn_gaussian_learning_rate = .0003;
+dbn_num_epochs = 50;
+dbn_song_batch_size = 15;
+dbn_mini_batch_size = 100;
+dbn_momentum = .6;
+dbn_binary_learning_rate = .1;
+dbn_gaussian_learning_rate = .0004;
 % nn
-nn_train_percentage = 30; % 30, 60, or 90. But keep it low.
-nn_num_epochs = 2;
-nn_batch_size = 2;
-nn_learning_rate = 1;
-nn_activation_function = 'sigm';
+nn_train_percentage = 30; % 30, 60, or 90. 
+nn_song_batch_size = 10;
+nn_num_epochs = 50;
+nn_batch_size = 100;
+nn_learning_rate = .6;
+nn_activation_function = 'sigm'; % 'tanh_opt' or 'sigm'
 nn_momentum = .6;
 nn_plot = 1;
 nn_output = 'softmax';
-nn_scaling_learning_rate = .99;
-nn_weight_penalty_L2 = .001;
-nn_non_sparsity_penalty = .1;
+nn_scaling_learning_rate = 1;
+nn_weight_penalty_L2 = .0001;
+nn_non_sparsity_penalty = .001;
 nn_sparsity_target = .05; % does nothing if above is set to 0
 nn_input_zero_masked_fraction = 0; % only non-zero for autoencoders
-nn_dropout_fraction = .5;
+nn_dropout_fraction = .6;
 
 % train the dbn and nn
 model_filename = create_train_save_beatles_dbn_nn_model(...
@@ -45,8 +50,7 @@ model_filename = create_train_save_beatles_dbn_nn_model(...
     nn_batch_size, nn_learning_rate, nn_activation_function, nn_momentum,...
     nn_plot, nn_output, nn_scaling_learning_rate, nn_weight_penalty_L2,...
     nn_non_sparsity_penalty, nn_sparsity_target,...
-    nn_input_zero_masked_fraction, nn_dropout_fraction);
-
+    nn_input_zero_masked_fraction, nn_dropout_fraction, nn_song_batch_size);
 
 
 % CREATE TRAINING AND TESTING DATA FOR SVM.
@@ -57,7 +61,8 @@ model_filename = create_train_save_beatles_dbn_nn_model(...
 %
 % Look at layers param to this function to see how to select only certain
 % layers from the NN
-model_filename = ['rbm_dbn_' model_filename];
+
+%layers = [2, 3, 4, 5];
 convert_trained_dbn_to_svm_efficient(model_filename);
 
 
