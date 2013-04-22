e0abb32998559d2baed46b038234722f89ea85fe
Samuel Messing <samuel.messing@gmail.com>
2013-04-22 01:32:22 -0400

    more set_up_path cleanup.

--- set_up_path.m
+++ set_up_path.m
@@ -1,24 +1,23 @@
-% Prepare matlab path for STFT code.
+% Prepare matlab path for our codebase and dependencies.
 %
 % NOTE: Assumes being run from top-level of repo.
 %
-%
-%function set_up_path
-  disp(sprintf('setting up paths...')) ;
+IS_DIR = 7 ;
+disp(sprintf('setting up paths...')) ;
 
-  path('lib/audioread', path) ;
-  path('lib/fuf', path) ;
-  addpath(genpath('lib/DeepLearnToolbox'));
-  path('src/dbn', path) ;
-  path('src/nn', path) ;
-  path('src/postprocessing/', path) ;
-  path('src/preprocessing/', path) ;
-  path('src/scripts/', path) ;
-  if exist('src/scripts/smrz') == 7
-    path('src/scripts/smrz/', path) ;
-  end
-  path('src/svm_hmm', path) ;
-  path('src/util/', path) ;
-  path('src/wrappers', path) ;
+path('lib/audioread', path) ;
+path('lib/fuf', path) ;
+addpath(genpath('lib/DeepLearnToolbox'));
+path('src/dbn', path) ;
+path('src/nn', path) ;
+path('src/postprocessing/', path) ;
+path('src/preprocessing/', path) ;
+path('src/scripts/', path) ;
+if exist('src/scripts/smrz') == IS_DIR
+  path('src/scripts/smrz/', path) ;
+end
+path('src/svm_hmm', path) ;
+path('src/util/', path) ;
+path('src/wrappers', path) ;
 
-  disp(sprintf('...done')) ;
+disp(sprintf('...done')) ;
